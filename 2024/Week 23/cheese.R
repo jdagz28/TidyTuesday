49d10b8fd0bb3030602509272050a72bda052888
jdagz28 <joshdagoy28@gmail.com>
2024-06-14 06:43:29 +0200

    from Windows

--- 2024/Week 23/cheese.R
+++ 2024/Week 23/cheese.R
@@ -10,15 +10,15 @@ library(forcats)
 library(scales)
 library(prismatic)
 
+setwd("~/++documents/TidyTuesday/2024/Week 23")
+
 # Load Data
 tuesdata <- tidytuesdayR::tt_load(2024, week = 23)
 cheeses <- tuesdata$cheeses
 
 # Data Check
-colnames(harvest_2020)
-glimpse(harvest_2020)
-skim(harvest_2020)
+colnames(cheeses)
+glimpse(cheeses)
+skim(cheeses)
 
-colnames(planting_2020)
-glimpse(planting_2020)
-skim(planting_2020)
+
